21910f105b9ffc904616253b5608acc7106a5e0d
Ling Song <lingsong.c4@gmail.com>
2026-05-28 12:18:25 +0800

    cleanup

--- app/app/src/main/java/com/example/modulo/MainActivity.kt
+++ app/app/src/main/java/com/example/modulo/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -21,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -77,6 +79,17 @@ fun SignInPage() {
     )
 }
 
+@Composable
+fun HomePage() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text("HomePage")
+    }
+}
+
 @Preview(
     showBackground = true,
     showSystemUi = false
@@ -84,6 +97,6 @@ fun SignInPage() {
 @Composable
 fun LayoutPreview() {
     ModuloTheme {
-
+        HomePage()
     }
 }
