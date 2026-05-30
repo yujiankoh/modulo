24623eb153e6f2483b1450c1e7ff169d7212b524
Ling Song <lingsong.c4@gmail.com>
2026-05-30 22:39:26 +0800

    changed layout
    removed counter, added assignment name and type

--- app/app/src/main/java/com/example/modulo/HomePage.kt
+++ app/app/src/main/java/com/example/modulo/HomePage.kt
@@ -1,23 +1,32 @@
 package com.example.modulo
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.modulo.ui.theme.ModuloTheme
 
 @Composable
 fun HomePage(
@@ -51,23 +60,70 @@ fun HomePage(
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Count: ${appData.counter}")
+        OutlinedTextField(
+            state = rememberTextFieldState(),
+            label = { Text("Title") }
+        )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(12.dp))
 
-        Button(
-            onClick = {
-                viewModel.incrementCounter()
-            }
+        Row(
+            modifier = Modifier
         ) {
-            Text("Increase Counter")
+            TaskTypeDropDownMenu()
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
 
         if (isDriveSyncEnabled) {
             Text(text = statusText)
         }
 
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskTypeDropDownMenu() {
+    var isExpanded by remember { mutableStateOf(false) }
+    val taskTypes = arrayOf("Assignment", "Exam", "Quiz")
+    var selectedText by remember { mutableStateOf(taskTypes[0]) }
+
+    ExposedDropdownMenuBox(
+        expanded = isExpanded,
+        onExpandedChange = { isExpanded = !isExpanded }
+    ) {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = {},
+            readOnly = true,
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded) },
+            modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable),
+        )
+
+        ExposedDropdownMenu(
+            expanded = isExpanded,
+            onDismissRequest = { isExpanded = false }
+        ) {
+            taskTypes.forEach { item ->
+                DropdownMenuItem(
+                    text = { Text(text = item) },
+                    onClick = {
+                        selectedText = item
+                        isExpanded = false
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun LayoutPreview() {
+    ModuloTheme {
+        HomePage(isDriveSyncEnabled = false)
     }
 }
